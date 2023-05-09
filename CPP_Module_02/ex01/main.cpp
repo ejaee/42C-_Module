89ed42a7a46c0501e5ae585cd3cf77bb123ee7ae
choiejae <ejae8319@gmail.com>
2023-05-09 12:32:54 +0900

    solved cpp02 except ex03

--- CPP_Module_02/ex01/main.cpp
+++ CPP_Module_02/ex01/main.cpp
@@ -6,19 +6,13 @@ int main( void ) {
   Fixed const c( 42.42f );
   Fixed const d( b );
 
-  std::cout << "-------------------------" << std::endl;
-
   a = Fixed( 1234.4321f );
-
-  std::cout << "-------------------------" << std::endl;
 
   std::cout << "a is " << a << std::endl;
   std::cout << "b is " << b << std::endl;
   std::cout << "c is " << c << std::endl;
   std::cout << "d is " << d << std::endl;
 
-  std::cout << "-------------------------" << std::endl;
-    
   std::cout << "a is " << a.toInt() << " as integer" << std::endl;
   std::cout << "b is " << b.toInt() << " as integer" << std::endl;
   std::cout << "c is " << c.toInt() << " as integer" << std::endl;
