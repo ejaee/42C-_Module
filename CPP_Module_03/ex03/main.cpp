3a6329d26fa9e63d6eacb687b0c736dae5e4f8b3
ejaee <ejae8319@gmail.com>
2023-05-18 15:21:16 +0900

    ready to eval ex03

--- CPP_Module_03/ex03/main.cpp
+++ CPP_Module_03/ex03/main.cpp
@@ -1,40 +1,56 @@
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
-int main() {
+int main(void) {
     std::string nameA("marine");
     std::string nameB("zergling");
     std::string nameC("zealot");
-    std::cout << "------------------------------------" << std::endl;
 
-    FragTrap fragTrapA(nameA);
-    FragTrap fragTrapB(nameB);
-    std::cout << "------------------------------------" << std::endl;
+    DiamondTrap diamondTrap1(nameA);
 
-    ClapTrap ClapTrapC(nameC);
-    std::cout << "------------------------------------" << std::endl;
+    std::cout << YELLOW << "\nPrint [ marine ] Status\n" << std::endl;
+
+    std::cout << "Name: " << diamondTrap1.getName() << std::endl;
+    std::cout << "Hit Points: " << diamondTrap1.getHitPoints() << std::endl;
+    std::cout << "Energy Points: " << diamondTrap1.getEnergyPoints() << std::endl;
+    std::cout << "Attack Damage: " << diamondTrap1.getAttackDamage() << RESET << std::endl;
+
+    std::cout << std::endl;
+
+    DiamondTrap diamondTrap2(nameB);
+
+    std::cout << std::endl;
+
+    ClapTrap clapTrap3(nameC);
+
+    std::cout << std::endl;
+
+    DiamondTrap diamondTrap4;
+
+    std::cout << std::endl;
+
+    diamondTrap1.attack(nameB);
+    diamondTrap2.takeDamage(diamondTrap1.getAttackDamage());
+    diamondTrap1.attack(nameB);
+    diamondTrap2.takeDamage(diamondTrap1.getAttackDamage());
+    diamondTrap1.attack(nameB);
+    diamondTrap2.takeDamage(diamondTrap1.getAttackDamage());
+    std::cout << std::endl;
+
+
+    diamondTrap1.attack(nameC);
+    clapTrap3.takeDamage(diamondTrap1.getAttackDamage());
+    diamondTrap1.attack(nameC);
+    clapTrap3.takeDamage(diamondTrap1.getAttackDamage());
+    diamondTrap1.attack(nameC);
+    clapTrap3.takeDamage(diamondTrap1.getAttackDamage());
+    std::cout << std::endl;
     
-    fragTrapA.beRepaired(20);
-    std::cout << "------------------------------------" << std::endl;
+    diamondTrap2.beRepaired(diamondTrap1.getAttackDamage() / 2);
+    diamondTrap2.beRepaired(diamondTrap1.getAttackDamage() / 2);
+    std::cout << std::endl;
 
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    fragTrapB.attack(nameA);
-    fragTrapA.takeDamage(fragTrapB.getAttackDamage());
-    std::cout << "------------------------------------" << std::endl;
-
-    fragTrapA.highFivesGuys();
-    fragTrapB.highFivesGuys();
-    fragTrapB.highFivesGuys();
-    std::cout << "------------------------------------" << std::endl;
-
+    diamondTrap1.highFivesGuys();
+    diamondTrap2.highFivesGuys();
+    
+  return 0;
 }
